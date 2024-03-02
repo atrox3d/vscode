728fb1b4f886e48074169b2808625926463a2cc6
atrox3d <atrox3d@gmail.com>
2024-03-02 18:31:30 +0100

    fix action

--- update-projects.sh
+++ update-projects.sh
@@ -7,25 +7,26 @@ grep -q $ACTION <<< "push pull" || die
 
 function update()
 {
-    [ $# -lt 2 ] && RECURSE=0 || ((RECURSE++))
+    [ $# -lt 3 ] && RECURSE=0 || ((RECURSE++))
     [ $RECURSE -lt 3 ] || { echo "MAX RECURSION REACHED $RECURSE";echo "RETURN";return; }
 
     cd "$1"
     echo "cwd=$PWD"
+    action=$2
 
     if [ -d .git ]
     then
-        echo "GIT:OK PULL   $1"
-        git pull
+        echo "GIT:OK ${action^^}   $1"
+        git $action
     else
         shopt -s nullglob
         for d in */
         do
             echo "GIT:KO UPDATE $d RECURSE"
-            (update $d RECURSE)
+            (update $d $action RECURSE)
         done
     fi
 }
 export -f update
 
-jq -c '.folders[]|.path' code-workspace.code-workspace | xargs -L1 -I% bash -c 'update %'
+jq -c '.folders[]|.path' code-workspace.code-workspace | xargs -L1 -I% bash -c "update % $ACTION"
