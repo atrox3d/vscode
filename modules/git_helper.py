68b5ef9d1436d11e0688f8074f8c5c8bcf1e665a
atrox3d <atrox3d@gmail.com>
2024-03-18 13:34:57 +0100

    git get_remote

--- modules/git_helper.py
+++ modules/git_helper.py
@@ -1,9 +1,24 @@
 from pathlib import Path
+import subprocess
+import os
 
 
 def is_repo(path:str) -> bool:
     gitdir =  (Path(path).resolve() / '.git')
     return gitdir.is_dir()
 
+def get_remote(path:str) -> str:
+    os.chdir(Path(path).resolve())
+    # print(os.getcwd())
+    command = 'git remote -v'.split()
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(result.stdout.split('\n'))
+    if result.returncode == 0:
+        print(result.stdout.split('\n')[0].split())
+        name, url, mode = result.stdout.split('\n')[0].split()
+        print(url)
+
 if __name__ == '__main__':
-    print(is_repo('../zio/cryptedit'))
+    path = '../zio/cryptedit/'
+    print(is_repo(path))
+    get_remote(path)
