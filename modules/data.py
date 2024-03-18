cf9ba89714b65a7cf4ef33018e4e16be3845dc20
atrox3d <atrox3d@gmail.com>
2024-03-18 12:41:18 +0100

    add get_folders

--- modules/data.py
+++ modules/data.py
@@ -4,13 +4,15 @@ from pathlib import Path
 class Workspace:
     def __init__(self, path:str) -> None:
         self.path = Path(path).resolve()
-
         with open(str(self.path)) as fp:
             self.data = json.load(fp)
-        
-        self.folders = self.data['folders']
+
+    def get_folders(self, resolve=True) -> list[str]:
+        folders = [folder['path'] for folder in self.data['folders']]
+        if resolve:
+            return [str(Path(folder).resolve()) for folder in folders]
 
 if __name__ == '__main__':
     ws = Workspace('code-workspace.code-workspace')
-    print(ws.folders)
+    print(ws.get_folders())
     
