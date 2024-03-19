35d889a4630cd7c0b27109d30164a80445ea9a83
atrox3d <atrox3d@gmail.com>
2024-03-19 16:58:01 +0100

    fix and cleanup

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from modules import git_helper as git
 if __name__ == '__main__':
     import os
     ws = Workspace('code-workspace.code-workspace')
-    for name, repo in ws.get_repos(recurse=True, absolute=False):
-        print(name, repo, git.get_remote(repo))
+    for name, repo in ws.get_repos(recurse=False, absolute=False):
+        print(name, repo, git.get_remote(repo) if repo is not None else repo)
--- modules/vscode_workspace.py
+++ modules/vscode_workspace.py
@@ -13,31 +13,28 @@ class Workspace:
         return self.data['folders']
 
     def get_tuples(self, default_name=None) -> list[tuple[str, str]]:
-        return [(folder.get('name', default_name), folder['path']) for folder in self.get_items()]
+        return [(folder.get('name', default_name), folder['path']) 
+                for folder in self.get_items()]
 
     def get_names(self, default_name=None) -> list[str]:
-        # return [folder.get('name', None) for folder in self.data['folders']]
-        return [item[0] for item in self.get_tuples(default_name)]
+        return [name for name, path in self.get_tuples(default_name)]
 
     def get_folders(self, absolute=True) -> list[str]:
-        # folders = [folder['path'] for folder in self.data['folders']]
-        folders = [item[1] for item in self.get_tuples()]
+        folders = [path for name, path in self.get_tuples()]
         if absolute:
             return [str(Path(folder).resolve()) for folder in folders]
         return folders
     
     def get_repos(self, absolute=False, default_name=None, recurse=True):
         for name, path in self.get_tuples(default_name):
-            path = Path(path)
-            if absolute:
-                path = path.resolve()
+            path = Path(path).resolve() if absolute else Path(path)
             if recurse:
                 for gitrepo in path.glob('**/.git/'):
                     yield name, gitrepo.parent
             elif git.is_repo(path):
                 yield name, path
             else:
-                yield name, 'not a repo'                
+                yield name, None           
 
 
 if __name__ == '__main__':
