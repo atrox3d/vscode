0b37e5c89fb34c5e143d5d5109d921deb327c97a
atrox3d <atrox3d@gmail.com>
2024-03-21 11:48:22 +0100

    working on status

--- modules/git_helper.py
+++ modules/git_helper.py
@@ -35,19 +35,84 @@ def get_remote(path:str) -> str:
     else:
         return None
 
+@dataclass
+class GitStatus:
+    '''
+    4.2. Status Flags
+    Furthermore, the status flag consists of two characters. 
+    If we're not performing a merge operation, 
+    the first character shows the index status 
+    while the second shows the workspace status.
+
+    The above example shows four different status flags:
+
+     M: modified
+     A: Added
+     D: deleted
+    ??: unknown to the index
+    
+    Additionally, the -porcelain option accepts a version property with two 
+    available values, 1 and 2. 
+    If we don't set a value, version 1 is the default. 
+    Also, version 1 is guaranteed not to change in the future 
+    and is more concise than version 2. 
+    As a result, it's easier and safer to use it within a script.
+    '''
+    branch: str
+    remote: str
+    position: str
+    modified: list
+    added: list
+    deleted: list
+    untracked: list
+    dirty: bool
+    push: bool
+    pull: bool
+
+import re
 def status(repo:GitRepo):
     '''
     git status --branch --porcelain
     ## master...origin/master [ahead 4]
     M modules/git_helper.py
     '''
-    command = 'git status --branchx --porcelain'
+    command = 'git status --branch --porcelain'
     result = git_command.run(command, repo.path)
-    if result.stdout:
-        for line in result.stdout.split('\n'):
-            print(line)
-    if result.stderr:
-        for line in result.stderr.split('\n'):
-            print(line)
+
+    branchstatus, *lines =  result.stdout.split('\n')
+    branch_pattern = r'^## ([^ .]+)\.{0,3}(\S*)( \[{0,1}(\S+) (\d+)\]{0,1})*$'
+    res = re.match(branch_pattern, branchstatus).groups()
+    print(f'{res = }')
+    branch, remote, _, position, n = res
+    if position == 'ahead':
+        push = True
+        pull = False
+    elif position == 'behind':
+        push = False
+        pull = True
     else:
-        return None
+        push = False
+        pull = False
+    
+    added = []
+    modified = []
+    deleted = []
+    untracked = []
+    dirty = False
+    for line in lines:
+        if len(line):
+            index = line[0]
+            workspace = line[1]
+            filename = line[2:]
+            dirty = True
+            match workspace:
+                case 'A':
+                    added.append(filename)
+                case 'M':
+                    modified.append(filename)
+                case 'D':
+                    deleted.append(filename)
+                case '?':
+                    untracked.append(filename)
+    status = GitStatus(branch, remote, position, modified, added, deleted, untracked, dirty, push, pull)
+    print(status)
