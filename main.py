7f29bd851cd0274867d6897687a248cf915b12be
atrox3d <atrox3d@gmail.com>
2024-04-03 21:29:20 +0200

    add update_projects

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 
 import test_simplegit
 import test_modules
+import update_projects
 
 if __name__ == '__main__':
-    sys.exit(test_simplegit.main())
+    sys.exit(update_projects.main())
 
