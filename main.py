119691839c4bcf5a32752779725f31fee1ff9419
atrox3d <atrox3d@gmail.com>
2024-04-11 17:30:46 +0200

    add help message

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ if __name__ == '__main__':
         'simplegit': test_simplegit,
         'modules': test_modules
     }
-    parser.add_argument('choice',choices=choices.keys())
+    parser.add_argument('choice',choices=choices.keys(), nargs='?')
 
     # parse just 1st arg, leave the rest for the chosen module
     parser.add_argument('rest', nargs='*')
     args, other = parser.parse_known_args()
-    sys.argv.pop(1)
-
-    print(f'{args = }')
-    for k, v in vars(args).items():
-        print(f'{k} = {v}')
     
-    sys.exit(choices[args.choice].main())
+    if args.choice is not None:
+        sys.argv.pop(1)
+
+        print(f'{args = }')
+        for k, v in vars(args).items():
+            print(f'{k} = {v}')
+        
+        sys.exit(choices[args.choice].main())
+    else:
+        print('please specify a module as first param:')
+        for k, v in choices.items():
+            print(f'{k:10} -> {v.__name__}')
 
