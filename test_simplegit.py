1c5890db89d7224f531e3e57e131658a6a91b23e
atrox3d <atrox3d@gmail.com>
2024-04-15 06:18:33 +0200

    setup to debug git status

--- test_simplegit.py
+++ test_simplegit.py
@@ -1,8 +1,20 @@
 # from pathlib import Path
+import argparse
 
 # from vscode_workspace import VsCodeWorkspace
 from atrox3d.simplegit import git
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-a', '--add', action='store_true', default=False)
+    parser.add_argument('-c', '--commit', action='store_true', default=False)
+    parser.add_argument('-p', '--pull', action='store_true', default=False)
+    parser.add_argument('-P', '--push', action='store_true', default=False)
+    parser.add_argument('-A', '--all', action='store_true', default=False)
+
+    return parser.parse_args()
 
 def print_status(status:git.GitStatus, repo:git.GitRepo) -> None:
     action = f'PUSH({status.commits})' if status.push else f'PULL({status.commits})' if status.pull else 'NO_ACTION'
@@ -16,25 +28,47 @@ def print_status(status:git.GitStatus, repo:git.GitRepo) -> None:
             )
 
 def main():
+
+    args = parse_args()
+    print(args)
     try:
+        print(f'GETTIG REPO FROM "."')
         path = '.'
         repo = git.get_repo(path)
         print(repo)
+
+        print(f'GETTIG STATUS FOR REPO "."')
         status = git.get_status(repo)
         print(status)
 
-        result = git.pull(path)
-        print(result)
+        if args.pull or args.all:
+            print(f'PULLING FROM {repo.remote}')
+            result = git.pull(path)
+            print(result)
+        else:
+            print(f'SKIPPING PULL')
 
         if status.dirty:
-            result = git.add(path, all=True)
-            print(result)
+            if args.add or args.all:
+                print(f'ADDING FILES')
+                result = git.add(path, all=True)
+                print(result)
+            else:
+                print(f'SKIPPING ADD')
 
-            result = git.commit(path, 'test commit', add_all=True)
-            print(result)
+            if args.commit or args.all:
+                print(f'COMMITTING CHANGES')
+                result = git.commit(path, 'test commit', add_all=True)
+                print(result)
+            else:
+                print(f'SKIPPING COMMIT')
 
-            result = git.push(path)
-            print(result)
+            if args.push or args.all:
+                print(f'PUSHING CHANGES')
+                result = git.push(path)
+                print(result)
+            else:
+                print(f'SKIPPING PUSH')
 
     except git.GitCommandException as gce:
         print(gce)
